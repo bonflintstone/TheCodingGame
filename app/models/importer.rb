cb4b6d0d7b79e6456010b121746cceacfbcfc38c
Bonflintstone <freddy@railslove.com>
2019-04-07 11:35:16 +0200

    Import users

--- app/models/importer.rb
+++ app/models/importer.rb
@@ -9,17 +9,26 @@ class Importer
     [GameConfig, Level, Step, Question, Answer]
       .map(&:destroy_all)
 
-    load_config
-    load_data
+    import_users
+    import_config
+    import_game
   end
 
-  def load_config
+  def import_users
+    @game_config[:predefinedUsers].each do |user|
+      User
+        .find_or_create_by(email: user[:username])
+        .update(password: user[:password])
+    end
+  end
+
+  def import_config
     GameConfig::KEYS.each do |key|
       GameConfig.create(key: key, value: @game_config[key.camelcase(:lower)])
     end
   end
 
-  def load_data
+  def import_game
     @game_config.fetch(:levels, []).each do |level|
       dbLevel = Level.create!(
         identifier: level[:id],
@@ -29,38 +38,46 @@ class Importer
         next_level: level[:nextLevel]
       )
 
-      level.fetch(:steps, []).each do |step|
-        dbStep = Step.create!(
-          identifier: step[:id],
-          level: dbLevel,
-          file1_name: step[:file1][:label],
-          file1_content: read_file('./app/models/answer.rb'),
-          file1_clarification: step[:file1][:path],
-          file2_name: step[:file2][:label],
-          file2_content: read_file('./app/models/question.rb'),
-          file2_clarification: step[:file2][:path]
-        )
-
-        step.fetch(:questions, []).each do |question|
-          dbQuestion = Question.create!(
-            step: dbStep,
-            text: question[:question],
-            correct_answer: question[:correctAnswer]
-          )
-
-          question.fetch(:answers, []).each do |answer|
-            Answer.create!(
-              text: answer[:text],
-              points: answer[:points],
-              question: dbQuestion
-            )
-          end
-        end
-      end
+      import_steps(level, dbLevel)
     end
   end
 
-  def read_file(path)
-    File.read(path)
+  def import_steps(level, dbLevel)
+    level.fetch(:steps, []).each do |step|
+      dbStep = Step.create!(
+        identifier: step[:id],
+        level: dbLevel,
+        file1_name: step[:file1][:label],
+        file1_content: File.read('./app/models/answer.rb'),
+        file1_clarification: step[:file1][:path],
+        file2_name: step[:file2][:label],
+        file2_content: File.read('./app/models/question.rb'),
+        file2_clarification: step[:file2][:path]
+      )
+
+      import_questions(step, dbStep)
+    end
+  end
+
+  def import_questions(step, dbStep)
+    step.fetch(:questions, []).each do |question|
+      dbQuestion = Question.create!(
+        step: dbStep,
+        text: question[:question],
+        correct_answer: question[:correctAnswer]
+      )
+
+      import_answers(question, dbQuestion)
+    end
+  end
+
+  def import_answers(question, dbQuestion)
+    question.fetch(:answers, []).each do |answer|
+      Answer.create!(
+        text: answer[:text],
+        points: answer[:points],
+        question: dbQuestion
+      )
+    end
   end
 end
