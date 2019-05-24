346bcc92a43844f457f4a0bdb58f3de72d8ea12b
Bonflintstone <freddy@railslove.com>
2019-05-24 23:30:50 +0200

    Add admin basic auth

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,8 @@
 require 'zip'
 
 class AdminController < ApplicationController
+  http_basic_authenticate_with name: 'admin', password: ENV['ADMIN_PASSWORD'] || 'local'
+
   def index
     @users = User.all
     @levels = Level.all
